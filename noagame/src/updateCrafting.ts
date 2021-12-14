a69ba51c066590587b3ae3a4888d085bb713af0e
Meow VPS <undefined@null.com>
2021-12-14 15:27:58 +0000

    add shaped recipes

--- noagame/src/updateCrafting.ts
+++ noagame/src/updateCrafting.ts
@@ -14,6 +14,33 @@ const singleRecipes = [
     amount: 1,
   },
 ];
+const shapedRecipes = [
+  {
+    req: { d: blocks.dirt.id },
+    out: blocks.grass.id,
+    amount: 1,
+    shape: `d\nd`,
+  },
+];
+
+export function gridToString() {
+  let getInv = (i: number) =>
+    (craftingInv.in[i] ? craftingInv.in[i][0].id : 0) || 0;
+  return `${getInv(0)}.${getInv(1)}
+${getInv(3)}.${getInv(2)}`;
+}
+export function gridCleanString() {
+  // fancy regex to replace ".0", ".0.", and "0." except at the start without replacing something like "10."
+  let fixed = ("\n" + gridToString())
+    .replace(/0{1}\.0{1}/g, "")
+    .replace(/\.?(?<!\w)(?<!\n)0{1}\.?/g, "")
+    .trim();
+  if (!fixed.split("\n").find((a) => !a.startsWith("0."))) {
+    // detects if all lines start with `0.` and shifts them left if so
+    fixed = fixed.replace(/0\.(?!0)/g, "");
+  }
+  return fixed;
+}
 
 export function updateCrafting() {
   let got = false;
@@ -21,6 +48,19 @@ export function updateCrafting() {
     if (got) return;
     let filtered = craftingInv.in.filter((c) => c);
     if (filtered.length == 1 && filtered[0][0].id == r.req) {
+      got = true;
+      return setInventoryItem(getBlock(r.out), 0, "craftingout", r.amount, "=");
+    }
+    setInventoryItem(null, 0, "craftingout");
+  });
+  shapedRecipes.forEach((r) => {
+    if (got) return;
+    let realShape = r.shape;
+    Object.keys(r.req).forEach((k) => {
+      realShape = realShape.replace(new RegExp(k, "g"), r.req[k]);
+    });
+    let filtered = gridCleanString();
+    if (realShape == filtered) {
       got = true;
       return setInventoryItem(getBlock(r.out), 0, "craftingout", r.amount, "=");
     }
